5e2b936dd33c0dbf41d3643a21e1cc3392ec84b9
SamuelRodri <samuelrh2001@hotmail.com>
2022-06-18 19:03:04 +0100

    Class 'Country'  created

---
+++ Assets/Scripts/Country.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshCollider), typeof(MeshFilter), typeof(MeshRenderer))]
+public class Country : MonoBehaviour
+{
+    public string admin;
+    public string ISO_A3;
+
+    public Country(string admin, string iso)
+    {
+        this.admin = admin;
+        this.ISO_A3 = iso;
+    }
+}
